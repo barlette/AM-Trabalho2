52ce4807ee093c7d841bd5e269a1094059530f35
barlette <leobarlette@gmail.com>
2018-05-18 15:07:46 -0300

    Implementado o correto parsing dos arquivos dos pesos e do número de camadas/neuronios

---
+++ ex1.py
@@ -0,0 +1,32 @@
+import sys
+import numpy
+import pandas
+
+network = open(sys.argv[1], "r")
+weights = open(sys.argv[2], "r")
+
+layers = network.readlines()
+
+regFactor = float(layers[0])
+
+layers.pop(0)
+
+for layer in range(len(layers)):
+    layers[layer] = float(layers[layer])
+    
+print(layers)
+
+weightLines = weights.readlines()
+
+wMatrix = numpy.zeros((0,0))
+
+for layer in range(len(weightLines)):
+	weightLines[layer] = weightLines[layer].split(';')
+	print(len(weightLines[layer]))
+	for inputs in range(len(weightLines[layer])):
+		weightLines[layer][inputs] = weightLines[layer][inputs].split(',')		
+		for nW in range(len(weightLines[layer][inputs])):
+			weightLines[layer][inputs][nW] = float(weightLines[layer][inputs][nW])
+
+print(weightLines)
+
