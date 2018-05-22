27ac1b0a0a3515f48edeaf82b7b1c20fd21c4595
barlette <leobarlette@gmail.com>
2018-05-22 15:59:20 -0300

    Forward Propagation parcialmente implementada.

--- ex1.py
+++ ex1.py
@@ -1,5 +1,5 @@
 import sys
-import numpy
+import numpy as np
 import pandas
 import csv
 
@@ -9,7 +9,6 @@ network = open(sys.argv[1], "r")
 weights = open(sys.argv[2], "r")
 
 data = pandas.read_csv(sys.argv[3], sep = ',', header = 0)
-print(data)
 
 layers = network.readlines()
 
@@ -19,27 +18,39 @@ layers.pop(0)
 
 for layer in range(len(layers)):
     layers[layer] = float(layers[layer])
-    
-print(layers)
 
 weightLines = weights.readlines()
 
-wMatrix = numpy.zeros((0,0))
+wMatrix = np.zeros((0,0))
 
 for layer in range(len(weightLines)):
 	weightLines[layer] = weightLines[layer].split(';')
-	print(len(weightLines[layer]))
+	#print(len(weightLines[layer]))
 	for inputs in range(len(weightLines[layer])):
 		weightLines[layer][inputs] = weightLines[layer][inputs].split(',')		
 		for nW in range(len(weightLines[layer][inputs])):
 			weightLines[layer][inputs][nW] = float(weightLines[layer][inputs][nW])
 
-
-print(weightLines)
-print("Input Neurons:", layers[0])
+#impressao das informações
+print(layers[0]-1, "input neurons")
 print("Network:")
+
 for layer in range(len(weightLines)):
 	print("Layer", layer+1)
-	for neuron in range(len(weightLines[layer])):
-		print("Neuron", neuron, ", Weight:", weightLines[layer][neuron])
+	for row in range(len(weightLines[layer])):
+		for neuron in range(len(weightLines[layer][row])):
+			print("Neuron", neuron+1, ", Weight:", weightLines[layer][row][neuron])
 
+#forward propagation
+inputs = np.matrix([[1], [2]])
+
+activations = []
+activations.append(inputs)
+
+print(activations[0])
+print(weightLines[0])
+print(np.matmul(weightLines[0], activations[0]))
+
+for layer in range(len(weightLines)):
+	activations.append(np.matmul(weightLines[layer], activations[layer]))
+	activations[layer+1] = np.vstack([1,activations[layer+1]])
