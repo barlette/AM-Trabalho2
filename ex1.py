f82c57c48e6632de242b1e920b6ec3720c86e840
barlette <leobarlette@gmail.com>
2018-05-23 16:00:16 -0300

    Função sigmóide e de normalização implementadas.

--- ex1.py
+++ ex1.py
@@ -1,14 +1,26 @@
 import sys
 import numpy as np
-import pandas
+import pandas as pd
 import csv
+import math
+
+def sigmoid(x):
+	return 1 / (1 + np.exp(-x))
+
+def normalize(df):
+	return (df - df.min()) / (df.max() - df.min())
 
 #parsing dos arquivos
 
 network = open(sys.argv[1], "r")
 weights = open(sys.argv[2], "r")
 
-data = pandas.read_csv(sys.argv[3], sep = ',', header = 0)
+
+data = pd.read_csv(sys.argv[3], sep = ',', header = 0)
+
+#print(data[1:3])
+norm = normalize(data)
+print(norm)
 
 layers = network.readlines()
 
@@ -42,15 +54,20 @@ for layer in range(len(weightLines)):
 			print("Neuron", neuron+1, ", Weight:", weightLines[layer][row][neuron])
 
 #forward propagation
+
+#print(norm[1:2])
+
 inputs = np.matrix([[1], [2]])
 
 activations = []
 activations.append(inputs)
 
-print(activations[0])
-print(weightLines[0])
-print(np.matmul(weightLines[0], activations[0]))
+#print(activations[0])
+#print(weightLines[0])
+#print(np.matmul(weightLines[0], activations[0]))
 
 for layer in range(len(weightLines)):
-	activations.append(np.matmul(weightLines[layer], activations[layer]))
+	activations.append(sigmoid(np.matmul(weightLines[layer], activations[layer])))
 	activations[layer+1] = np.vstack([1,activations[layer+1]])
+
+print(activations)
