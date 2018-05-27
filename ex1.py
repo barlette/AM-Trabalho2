0df2d8993d97d206b0983c7723013dcb6f63384d
barlette <leobarlette@gmail.com>
2018-05-27 00:07:06 -0300

    Backpropagation da camada de saída implementada.

--- ex1.py
+++ ex1.py
@@ -10,6 +10,9 @@ def sigmoid(x):
 def normalize(df):
 	return (df - df.min()) / (df.max() - df.min())
 
+def derivativeSig(x):
+	return np.multiply(x, (1-x))
+
 #parsing dos arquivos
 
 network = open(sys.argv[1], "r")
@@ -20,7 +23,7 @@ data = pd.read_csv(sys.argv[3], sep = ',', header = 0)
 
 #print(data[1:3])
 norm = normalize(data)
-print(norm)
+#print(norm)
 
 layers = network.readlines()
 
@@ -57,7 +60,8 @@ for layer in range(len(weightLines)):
 
 #print(norm[1:2])
 
-inputs = np.matrix([[1], [2]])
+inputs = np.matrix([[1], [0.05], [0.1]])
+outputs = np.matrix([[0.01], [0.99]])
 
 activations = []
 activations.append(inputs)
@@ -67,7 +71,43 @@ activations.append(inputs)
 #print(np.matmul(weightLines[0], activations[0]))
 
 for layer in range(len(weightLines)):
+	# print(weightLines[layer])
+	# print(activations[layer])
+	# print("teste1")
+	# print(np.matmul(weightLines[layer], activations[layer]))
 	activations.append(sigmoid(np.matmul(weightLines[layer], activations[layer])))
 	activations[layer+1] = np.vstack([1,activations[layer+1]])
 
 print(activations)
+
+#print(len(activations[len(activations)-1]))
+#print(activations[len(activations)-1][1:len(activations[len(activations)-1])])
+index = len(activations)-1
+outputActivations = activations[index][1:len(activations[index])]
+
+#totalError = np.sum(0.5*(np.power(outputs - activations[len(activations)-1],2)))
+#print("Total error: ",totalError)
+
+print(activations[index])
+error = outputActivations - outputs
+print("error:", error)
+
+slopeOut = np.zeros((len(outputActivations),1))
+slopeOut = derivativeSig(outputActivations)
+print("slopeOut:", slopeOut)
+
+delta = np.multiply(error,slopeOut)
+print("delta:", delta)
+#delta = np.ndarray.transpose(delta)
+#activationsPL = activations[index-1][1:len(activations[index-1])]
+activationsPL = activations[index-1]
+print(activationsPL)
+print(delta.T)
+gradiente = np.matmul(activationsPL, delta.T)
+print("der:", gradiente)
+
+print(weightLines[index-1])
+
+attWeight = weightLines[index-1] -(regFactor*gradiente).T
+print(attWeight)
+
